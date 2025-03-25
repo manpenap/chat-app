bb35a2a06509fe42a7adddad6d4ebd19eb0fd3bf
manuel peña <manpenap@gmail.com>
2025-03-25 00:03:57 -0300

    Agrega ruta para la raiz

--- chat-backend/server.js
+++ chat-backend/server.js
@@ -26,6 +26,11 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).json({ error: err.message || 'Internal Server Error' });
 });
 
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando correctamente 🚀");
+});
+
+
 // Iniciar el servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
