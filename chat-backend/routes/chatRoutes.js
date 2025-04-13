75499f89143d1f86079f2ca152d9327b12804533
manuel peña <manpenap@gmail.com>
2025-04-12 23:00:31 -0400

    modifica ruta

--- chat-backend/routes/chatRoutes.js
+++ chat-backend/routes/chatRoutes.js
@@ -5,7 +5,7 @@ import chatController from '../controllers/chatController.js';
 const router = express.Router();
 
 router.get('/welcome', chatController.getWelcomeMessage);
-router.get('/last-conversation', verifyToken, chatController.getLastConversation);
+router.get('/last-conversation', chatController.getLastConversation);
 router.post('/save-conversation', chatController.saveConversation);
 router.post('/', chatController.handleChat);
 
