d5dc1ae3eab4feb5194515a8d92b43794e5d6c55
manuel peña <manpenap@gmail.com>
2025-04-13 22:17:15 -0400

    Agrega protect a las rutas del chat

--- chat-backend/routes/chatRoutes.js
+++ chat-backend/routes/chatRoutes.js
@@ -1,12 +1,13 @@
 // chatRoutes.js
 import express from 'express';
 import chatController from '../controllers/chatController.js';
+import protect from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
 router.get('/welcome', chatController.getWelcomeMessage);
-router.get('/last-conversation', chatController.getLastConversation);
-router.post('/save-conversation', chatController.saveConversation);
+router.get('/last-conversation', protect,  chatController.getLastConversation);
+router.post('/save-conversation', protect, chatController.saveConversation);
 router.post('/', chatController.handleChat);
 
 export default router;
